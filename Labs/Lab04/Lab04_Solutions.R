40161f9903e8ba5091958f483e9267b1d965ef9b
Mike Ramsey <Michael.E.Ramsey@colorado.edu>
2019-02-06 11:26:12 -0700

    Added Solution File

---
+++ Labs/Lab04/Lab04_Solutions.R
@@ -0,0 +1,37 @@
+##### Michael Ramsey #####
+# Lab 4 Exercises
+# Solutions
+# 02/06/2019
+
+##### Exercise 1 #####
+
+# Load libraries
+library(tidyverse)
+
+# Load data
+data("diamonds")
+
+##### Exercise 2 #####
+
+# View the first few rows of data
+head(diamonds)
+
+# View all the data
+View(diamonds)
+
+# Structure of the data
+str(diamonds)
+
+##### Exercise 3 #####
+
+# Bar plot of cut class, grouped by clarity
+ggplot(data = diamonds, aes(x = cut, fill = clarity)) + 
+  geom_bar(position = 'dodge')
+
+##### Exercise 4 #####
+
+# Subset diamond data by 'ideal' cut
+df <- diamonds %>%
+  filter((cut == 'Ideal') & (clarity %in% c('SI1','SI2')))
+
+##### Exercise 5 #####
